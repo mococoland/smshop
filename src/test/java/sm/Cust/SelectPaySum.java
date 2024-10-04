83e4023136416aa75128684d2cbafc47740cca8a
YUN <krzlnt@gmail.com>
2024-10-04 10:06:04 +0900

    CUST 총 결제금액 조회

---
+++ src/test/java/sm/Cust/SelectPaySum.java
@@ -0,0 +1,24 @@
+package sm.Cust;
+
+import edu.sm.service.PayService;
+
+import java.sql.SQLException;
+
+public class SelectPaySum {
+    public static void main(String[] args) throws SQLException {
+        // PayService 인스턴스 생성
+        PayService payService = new PayService();
+
+        int custKey = 3; // 조회할 custKey (예: 3)
+
+        try {
+            // custKey에 해당하는 총 결제 금액 조회
+            double totalAmount = payService.getTotalPayAmount(custKey);
+
+            // 결과 출력
+            System.out.println("고객 ID " + custKey + "의 총 결제 금액: " + totalAmount);
+        } catch (Exception e) {
+            e.printStackTrace(); // 예외 처리
+        }
+    }
+}
